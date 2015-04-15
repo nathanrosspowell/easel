4ef02027d34d5277a34b00f4bdb5f8a17fdefa2b
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-04-14 22:04:51 -0400

    Remove DERP :facepunch:

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 window.onload = function init() {
-    console.log("DERP");
     var stage = new createjs.Stage("demoCanvas");    
     var circle = new createjs.Shape();
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
