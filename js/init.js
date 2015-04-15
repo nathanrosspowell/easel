16dc1890d46aa6d8738e2bfe4dd2ee0552715bd9
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-04-14 21:19:46 -0400

    Grunt deploy Tue Apr 14 2015 21:19:44

---
+++ js/init.js
@@ -0,0 +1,10 @@
+window.onload = function init() {
+    console.log("DERP");
+    var stage = new createjs.Stage("demoCanvas");    
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+    stage.update();
+}
