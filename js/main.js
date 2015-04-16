57baea5251b9689b6a3dc9b04b1fedfcabf813b7
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-04-15 23:41:06 -0400

    Grunt deploy Wed Apr 15 2015 23:41:04

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.onload = function init() {
+    // Test draw a circle
     var stage = new createjs.Stage("demoCanvas");    
     var circle = new createjs.Shape();
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
@@ -6,4 +7,12 @@ window.onload = function init() {
     circle.y = 100;
     stage.addChild(circle);
     stage.update();
+    // Load files
+    var preload = new createjs.LoadQueue();
+    preload.addEventListener("fileload", handleFileComplete);
+    preload.loadFile("kenney-hexagon/Spritesheet/complete.png");
+}
+
+function handleFileComplete(event) {
+  document.body.appendChild(event.result);
 }
